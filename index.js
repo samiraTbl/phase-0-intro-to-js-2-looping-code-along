a4eb7fc1eabd5471931e24ce66d62a1462fd338d
samira toubal seghir <samsidt@gmail.com>
2023-08-03 20:53:40 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+function writeCards(arr, evt)
+{const narr=[]
+for (let i=0;i<arr.length;i++)
+{
+    narr.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`)
+}
+return narr
+}
+function countDown(num)
+{
+    let i =num; // the initialization moves OUTSIDE the body of the loop!
+  while (i>=0) {
+    console.log(i);
+    i--; // the iteration moves INSIDE the body of the loop!
+  }
+}
+countDown(5)
